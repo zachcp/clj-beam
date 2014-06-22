a6589b62f733e17bead6362979e7a2808581a4b2
zachcp <zcharlop@rockefeller.du>
2014-06-22 18:28:25 -0400

    update regex

--- src/clj_beam/regex.clj
+++ src/clj_beam/regex.clj
@@ -12,7 +12,7 @@
 ; java regex reference here:
 ; http://www.tutorialspoint.com/java/java_regular_expressions.htm
 
-(def examples ["OH3+"  "35Cl:classinfo" "27Fe+2" "2H" "235U" "C@@H" "C@H" "I-" "Na+" "Br-" "H+" "Fe+2" "OH-" "Fe++" "OH3+" "NH4+"])
+(def examples ["OH3+" "Fe+++" "35Cl:classinfo" "27Fe+2" "2H" "235U" "C@@H" "C@H" "I-" "I-2" "Na+" "Br-" "H+" "Fe+2" "OH-" "Fe++" "OH3+" "NH4+"])
 
 (defn bracketatominfo [a]
   "a bracketed   atom can may have the followinwg:
@@ -23,29 +23,43 @@
      charge:     multiple +/- with optional digit
      class:      optional field to specify extra information"
   (let [;get basic charge, proton, and isotope information
-        element   (re-seq #"(?i)[abcdefgijklmnopqrstuvmwyz]+" a) ;no "H"
-        isotope   (re-seq #"^[0-9]+" a)
+        element   (let  [ el (re-seq #"(?i)[abcdefgijklmnopqrstuvmwyz]+" a) ]
+                    (if (nil? el)  "H" el)) ;pattern excludes "H" for ease. add it back if necessary
+        isotope   (re-find #"^[0-9]+" a)
         chiral    (re-seq #"[@]+" a)
         chiralH   (re-seq #"[@]+[H]" a)
-        charge    (re-seq #"[+-]+" a)
-        chargenum (re-seq #"[+-]+[0-9]" a)
+        charge    (first (re-seq #"[+-]+" a))
+        chargenum (first (map read-string (re-find #"[+-](\d)" a)))
         protons   (re-seq #"[H]+" a)
-        protnum   (re-seq #"[H][0-9]" a)
-        atomclass (re-seq #"[:](?i)(\w+)" a)
+        protnum   (second ( map read-string (re-find #"[H](\d)" a)))
+        atomclass (re-seq #":(?i)(\w+)" a)
 
         ;resolve ambiguities
+        fin_charge (cond
+                     chargenum chargenum
+                     charge  ( if (= (first charge) \+)
+                                  (count charge)
+                                  (- (count charge)))
+                    :else 0)
 
-        ;return an atom map contianing relevant information
-        tempatom  {}
+        fin_protons (cond
+                     protnum protnum
+                     protons 1
+                    :else 0)
         ]
-   (-> tempatom
-        (assoc :element (first element))
-        (assoc :isotope (first isotope)))
 
-  [a element isotope chiral chiralH charge chargenum protons protnum atomclass]
+    ;[a element isotope chiral chiralH charge chargenum fin_charge protons protnum atomclass]
+
+
+     { :element   (first element)
+       :isotope   isotope
+       :charge    fin_charge
+       :hydrogens fin_protons
+       :atomclass atomclass }
+
+;    [a protons protnum]
 
     ))
 
 
 (map bracketatominfo examples)
-
