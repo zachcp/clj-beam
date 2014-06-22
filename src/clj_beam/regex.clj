f7c4231a87e1df4d6e4a391cd76dc098c372e802
zachcp <zcharlop@rockefeller.du>
2014-06-22 17:22:41 -0400

    small mods

--- src/clj_beam/regex.clj
+++ src/clj_beam/regex.clj
@@ -9,7 +9,10 @@
 ; example Strings to pick out
 ; OH3+ 2H 235U C@@H C@H I- Na+ Br- H+ Fe+2 OH- Fe++ OH3+ NH4+
 
-(def examples ["OH3+"  "35Cl:classinfo" "2H" "235U" "C@@H" "C@H" "I-" "Na+" "Br-" "H+" "Fe+2" "OH-" "Fe++" "OH3+" "NH4+"])
+; java regex reference here:
+; http://www.tutorialspoint.com/java/java_regular_expressions.htm
+
+(def examples ["OH3+"  "35Cl:classinfo" "27Fe+2" "2H" "235U" "C@@H" "C@H" "I-" "Na+" "Br-" "H+" "Fe+2" "OH-" "Fe++" "OH3+" "NH4+"])
 
 (defn bracketatominfo [a]
   "a bracketed   atom can may have the followinwg:
@@ -20,8 +23,8 @@
      charge:     multiple +/- with optional digit
      class:      optional field to specify extra information"
   (let [;get basic charge, proton, and isotope information
-        element   (re-seq #"(?i)[abcdefghijklmnopqrstuvmwyz]+" a)
-        isotope   (re-seq #"(?i)[0-9]+[abcdefghijklmnopqrstuvmwyz]" a)
+        element   (re-seq #"(?i)[abcdefgijklmnopqrstuvmwyz]+" a) ;no "H"
+        isotope   (re-seq #"^[0-9]+" a)
         chiral    (re-seq #"[@]+" a)
         chiralH   (re-seq #"[@]+[H]" a)
         charge    (re-seq #"[+-]+" a)
@@ -29,8 +32,19 @@
         protons   (re-seq #"[H]+" a)
         protnum   (re-seq #"[H][0-9]" a)
         atomclass (re-seq #"[:](?i)(\w+)" a)
-        tempatom  {}]
-    [a element isotope chiral chiralH charge chargenum protons protnum atomclass]))
+
+        ;resolve ambiguities
+
+        ;return an atom map contianing relevant information
+        tempatom  {}
+        ]
+   (-> tempatom
+        (assoc :element (first element))
+        (assoc :isotope (first isotope)))
+
+  [a element isotope chiral chiralH charge chargenum protons protnum atomclass]
+
+    ))
 
 
 (map bracketatominfo examples)
